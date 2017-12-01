638810b616316b4f03e4a5900ed79307c37d0a17
na <ady.niko51@gmail.com>
2017-12-01 18:46:09 +0200

    first commit

---
+++ Project10/Source.cpp
@@ -0,0 +1,13 @@
+#include <afxwin.h>
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+	CWinApp MyApp;
+
+	CWnd MyWindow;
+
+	MyWindow.Create;
+
+	return MyApp.Run();
+}
